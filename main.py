5a8cca58c27587c2381b09a3a4ed3c8e4de907dc
AQ <qasemcs@gmail.com>
2023-11-15 10:22:54 -0500

    draft

---
+++ main.py
@@ -0,0 +1,9 @@
+""" includes the necessary functions to load and manipulate DICOM images """
+
+import os
+import sys
+import glob
+import pydicom as dicom
+from dicom_contour.contour import *
+import cv2
+
